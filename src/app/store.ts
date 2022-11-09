4305c329ddaba229371bc335e179783296f7a2e9
Marco Aurélio <marcocpdti@gmail.com>
2022-11-09 10:10:08 -0300

    initial commit login

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { api } from './services/auth'
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [api.reducerPath]: api.reducer,
+    auth: authReducer,
   },
 });
 

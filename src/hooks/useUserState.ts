b54519bbe2c0a2f3df2fbf9abbc3dd1402b16948
Marco Aurélio <marcocpdti@gmail.com>
2022-12-15 20:17:20 -0300

    adjusted types

--- src/hooks/useUserState.ts
+++ src/hooks/useUserState.ts
@@ -1,3 +1,4 @@
+import { User } from '../app/interfaces/authInterfaces';
 import { RootState } from '../app/store'
 
-export const selectUser = (state: RootState): string | null => state.auth.accessToken;
+export const selectUser = (state: RootState): User | null => state.auth.user;

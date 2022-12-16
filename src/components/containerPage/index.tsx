aa6872f560b67b91a857a9f6472a0f7aeb125fc7
Marco Aurélio <marcocpdti@gmail.com>
2022-12-15 23:38:16 -0300

    delete background color

--- src/components/containerPage/index.tsx
+++ src/components/containerPage/index.tsx
@@ -7,7 +7,7 @@ interface ChildrenProps {
 
 export const ContainerPage = ({ children }: ChildrenProps) => {
     return (
-        <Box bg="gray.100" h="calc(100vh - 72px)" p='8'>
+        <Box h="calc(100vh - 72px)" p='8'>
             {React.Children.map(children, (child) => (
                 child
             ))}
